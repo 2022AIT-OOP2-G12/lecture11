bf3c5ac40c1b1fb9b8beaa2db70b2b639a5e72b4
kk-r-on <kazu.2003.onoda@gmail.com>
2022-12-15 16:06:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from Watcher import watcher
+import pathlib
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
 
@@ -7,6 +8,12 @@ app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/upload')
+def upload():
+    input_dir = "./static/input_dir"
+    images = list(pathlib.Path(input_dir).glob('**/*.png'))
+    return render_template("upload_list.html",images=images)
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
